10e39749fbf65928d12d9887f344fdefbcd8e24e
Ronaldo <rfeitosa@ciandt.com>
2020-04-17 21:47:51 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+import "./styles.css";
+
+import api from './services/api';
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(res => {
+      setRepositories(res.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const res = await api.post('/repositories', {
+      title: `Novo Repositório - ${Date.now()}`,
+      url: 'url teste',
+      techs: ['NodeJS', 'ReactJs', 'React Native']
+    });
+
+    const repository = res.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repo => repo.id !== id))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repo => 
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        )}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
